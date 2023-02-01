8b25817cfbc47a8b909a2046b8beb36177b8ea92
Mark <mark@remarkablemark.org>
2023-02-01 18:58:21 -0500

    refactor(components): modularize Grid from Board

---
+++ src/components/Grid.tsx
@@ -0,0 +1,49 @@
+import { Move } from '../game';
+
+const cellStyle = {
+  border: '1px solid #555',
+  fontFamily: 'Helvetica, Arial',
+  width: '50px',
+  height: '50px',
+  lineHeight: '50px',
+  textAlign: 'center',
+};
+
+const numberToLetter = {
+  0: 'O',
+  1: 'X',
+};
+
+export default function Grid({ ctx, G, moves }) {
+  const tbody = [];
+
+  for (let i = 0; i < 3; i++) {
+    const cells = [];
+
+    for (let j = 0; j < 3; j++) {
+      const id = 3 * i + j;
+
+      cells.push(
+        <td key={id}>
+          {G.cells[id] ? (
+            <div style={cellStyle}>{numberToLetter[G.cells[id]]}</div>
+          ) : (
+            <button
+              style={cellStyle}
+              onClick={() => moves[Move.ClickCell](id)}
+              disabled={Boolean(ctx.gameover)}
+            />
+          )}
+        </td>
+      );
+    }
+
+    tbody.push(<tr key={i}>{cells}</tr>);
+  }
+
+  return (
+    <table>
+      <tbody>{tbody}</tbody>
+    </table>
+  );
+}
