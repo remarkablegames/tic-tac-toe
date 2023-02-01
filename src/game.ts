188806f9496dbd0cfcd9365fc3c7b285cd0434c3
Mark <mark@remarkablemark.org>
2023-01-31 19:41:56 -0500

    feat: manage turns in game
    https://boardgame.io/documentation/#/tutorial?id=managing-turns

--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,11 @@ export function setup() {
   return { cells: [0, 1, 2, 3, 4, 5, 6, 7, 8] };
 }
 
+export const turn = {
+  minMoves: 1,
+  maxMoves: 1,
+};
+
 function clickCell({ G, playerID }, id) {
   if (G.cells[id] !== null) {
     return INVALID_MOVE;
