40a57f5c2d32cc9b26736fe1e7a375588c936fca
Mark <mark@remarkablemark.org>
2023-01-31 19:41:03 -0500

    feat: validate move in game
    https://boardgame.io/documentation/#/tutorial?id=validating-moves

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,17 @@
+import { INVALID_MOVE } from 'boardgame.io/core';
+
 export function setup() {
   return { cells: [0, 1, 2, 3, 4, 5, 6, 7, 8] };
 }
 
+function clickCell({ G, playerID }, id) {
+  if (G.cells[id] !== null) {
+    return INVALID_MOVE;
+  }
+
+  G.cells[id] = playerID;
+}
+
 export const moves = {
-  clickCell: ({ G, playerID }, id) => {
-    G.cells[id] = playerID;
-  },
+  clickCell,
 };
